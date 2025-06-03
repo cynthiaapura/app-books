e921f0452418ac812374135ee6bc7c76633faf23
fiona Vr <fiovert1192@gmail.com>
2025-06-03 14:03:45 +0200

    filter

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -52,3 +52,19 @@ export const deleteBooks = async (req, res) => {
         res.status(400).json({ message: error.message });
     }
 }
+
+router.get('/', async (req, res) => {
+    try {
+        const { isbn, title, author } = req.query;
+
+        const filter = {};
+        if (isbn) filter.isbn = isbn;
+        if (title) filter.title = new RegExp(title, 'i'); 
+        if (author) filter.author = new RegExp(author, 'i');
+
+        const books = await Book.find(filter);
+        res.json(books);
+    } catch (error) {
+        res.status(500).json({ message: 'Erreur serveur', error: error.message });
+    }
+});
--- routes/books.js
+++ routes/books.js
@@ -16,20 +16,5 @@ router.put('/:id', updateBooks);
 router.delete('/:id', deleteBooks);
 
 
-router.get('/', async (req, res) => {
-    try {
-        const { isbn, title, author } = req.query;
-
-        const filter = {};
-        if (isbn) filter.isbn = isbn;
-        if (title) filter.title = new RegExp(title, 'i'); 
-        if (author) filter.author = new RegExp(author, 'i');
-
-        const books = await Book.find(filter);
-        res.json(books);
-    } catch (error) {
-        res.status(500).json({ message: 'Erreur serveur', error: error.message });
-    }
-});
 
 export default router;
